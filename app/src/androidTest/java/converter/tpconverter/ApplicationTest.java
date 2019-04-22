87054ea320e4baeb2e7403652e99773724773bb2
AdelinaStana <stana.adelina.diana@gmail.com>
2019-04-22 16:11:56 +0300

    Update ApplicationTest.java

--- app/src/androidTest/java/converter/tpconverter/ApplicationTest.java
+++ app/src/androidTest/java/converter/tpconverter/ApplicationTest.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 /**
- * this is a test
+ * this is a comment
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
